13e7c4bd9541375f3f19fd90a6d516ed3969123f
FaaizHaikal <faaizhaikal@gmail.com>
2024-12-17 18:16:51 +0700

    feat: add node hpp

---
+++ include/pinocchio_test/node/pinocchio_test_node.hpp
@@ -0,0 +1,34 @@
+#ifndef PINOCCHIO_TEST__NODE__PINOCCHIO_TEST_NODE_HPP_
+#define PINOCCHIO_TEST__NODE__PINOCCHIO_TEST_NODE_HPP_
+
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+
+#include "tachimawari_interfaces/msg/current_joints.hpp"
+#include "kansei_interfaces/msg/status.hpp"
+#include "pinocchio_test/model/robot_wrapper.hpp"
+
+namespace pinocchio_test
+{
+
+class PinocchioTestNode
+{
+public:
+  using CurrentJoints = tachimawari_interfaces::msg::CurrentJoints;
+  using KanseiStatus = kansei_interfaces::msg::Status;
+
+  PinocchioTestNode(const rclcpp::Node::SharedPtr & node, const std::string & urdf_path);
+
+  void publish_robot_state();
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  std::shared_ptr<RobotWrapper> robot_wrapper_;
+  rclcpp::Subscription<CurrentJoints>::SharedPtr current_joints_subscriber;
+  rclcpp::Subscription<KanseiStatus>::SharedPtr kansei_status_subscriber;
+
+};
+
+}  // namespace pinocchio_test
+
+#endif  // PINOCCHIO_TEST__NODE__PINOCCHIO_TEST_NODE_HPP_
